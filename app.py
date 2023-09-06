d46b07bb60db5b71568d5dac916b606ad12df748
AbdellahDeveloper <83137499+AbdellahDeveloper@users.noreply.github.com>
2023-09-06 12:03:36 -0700

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,25 @@
 from flask import Flask
-
+from flask import request
+import requests
+from bs4 import BeautifulSoup
+import json
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return 'Hello World!'
+@app.route('/',methods=["POST","GET"])
+def handle_request():
+    url=str(request.args.get("url"))
+    response = requests.get(url)
+    doc=BeautifulSoup(response,"html.parser")
+    JsonString=[] 
+    mydivs = doc.find_all("div", {"class": "views-row"})
+    for item in mydivs:
+        previewVideo=str(item.find("article").find_all("div")[0].find("div").find("a").find("video")['src'])
+        JsonString.append({
+          "title": item.find("article").find_all("div")[1].find("a").find("span").string,
+          "preview_video":"https://mocap.market"+previewVideo,
+          "download_link":"/download/"+previewVideo.split("/sites/default/files/")[1].split(".fbx")[0]+".fbx"
+        })
+    return json.dumps(JsonString)
 
 if __name__ == '__main__':
     app.run()
