20a8dbef98a42303cd4bf528a66af81c820809fc
AbdellahDeveloper <83137499+AbdellahDeveloper@users.noreply.github.com>
2023-09-07 11:06:42 -0700

    Update app.py

--- app.py
+++ app.py
@@ -27,7 +27,7 @@ def handle_request():
         return json.dumps({'AllAnimations':JsonString, 'tabscount':lastindex})
       else:
         return 'no data available'
-  except Exception:
-        return 'exeption, no data available'
+  except Exception, e:
+        return 'exeption, no data available'+str(s)
 if __name__ == '__main__':
     app.run()
